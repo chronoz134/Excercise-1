c119a2425b51da941f2c840b50b2cb4f6ccf0fdf
Zechariah Delossantos <60047211+chronoz134@users.noreply.github.com>
2022-09-10 14:13:51 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+ 		//variable initialization and declaration with values provided from pompt
+  	int firstnum = 50;
+  	int secondnum = 100;
+  
+  	//we declare total and give it the sum of the first two variables.
+  	int total = firstnum + secondnum;
+
+  	//printing out the sum of the first two vars that was placed into total
+  	std::cout << total;
+}
